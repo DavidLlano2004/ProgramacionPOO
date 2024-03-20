32ec5e28caa199200379fcab70ce57bb7a191a76
DavidLlano2004 <juli.matias.2004@gmail.com>
2024-03-20 11:55:35 -0500

    Cambio de linea

--- src/edu/caldas/poo/gradeBook/GradeBookTest.java
+++ src/edu/caldas/poo/gradeBook/GradeBookTest.java
@@ -4,5 +4,6 @@ public class GradeBookTest {
     public static void main(String[] args) {
         GradeBook courseName1 = new GradeBook("numero2");
         System.out.println(courseName1.displayMessage() + courseName1.getCourseName());
+
     }
 }
